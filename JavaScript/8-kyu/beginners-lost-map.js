cd6d3a1fce086ddf6a32379dc0a792b2ca838900
Caio Rocha <rochacaiodev@gmail.com>
2023-05-22 23:43:52 -0300

    add new finished exercises

---
+++ JavaScript/8-kyu/beginners-lost-map.js
@@ -0,0 +1,11 @@
+/*
+  Description:
+
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+*/
+
+const maps = (numbers) => numbers.map(num => num * 2)
