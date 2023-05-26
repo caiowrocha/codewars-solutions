c7a20dfa3bb67be74313f2ebd2dc15d1ba8820ae
Caio Rocha <rochacaiodev@gmail.com>
2023-05-26 00:41:21 -0300

    add some more exercises done

---
+++ JavaScript/6-kyu/duplicate-arguments.js
@@ -0,0 +1,28 @@
+/*
+Description:
+
+Complete the solution so that it returns true if it contains any duplicate argument values. Any number of arguments may be passed into the function.
+
+The array values passed in will only be strings or numbers. The only valid return values are true and false.
+
+Examples:
+
+solution(1, 2, 3)             -->  false
+solution(1, 2, 3, 2)          -->  true
+solution('1', '2', '3', '2')  -->  true
+*/
+
+const solution = (...arguments) => {
+
+  const box = new Array();
+
+  for(let i = 0; i < arguments.length; i++) {
+      let member = arguments[i];
+      box.push(member);
+  };
+
+  const noDupes = new Set(box);
+
+  return box.length !== noDupes.size;
+
+};
---
+++ JavaScript/8-kyu/third-angle-of-triangle.js
@@ -0,0 +1,12 @@
+/*
+
+You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested.
+
+*/
+
+
+const otherAngle = (x, y) => 180 - x - y;
