ea1949ee55b23ee15d44c6e20c2db2251c1bd01a
Caio Rocha <rochacaiodev@gmail.com>
2023-05-28 00:36:56 -0300

    add new solutions

---
+++ JavaScript/8-kyu/be-consice-index-of-element.js
@@ -0,0 +1,15 @@
+/*
+
+Description:
+Be Concise IV - Index of an element in an array
+Task
+
+Provided is a function Kata which accepts two parameters in the following order: array, element and returns the index of the element if found and "Not found" otherwise. Your task is to shorten the code as much as possible in order to meet the strict character count requirements of the Kata. (no more than 161) You may assume that all array elements are unique.
+
+
+*/
+
+
+const find = (a, e) => {
+  return a.indexOf(e) !== -1 ? a.indexOf(e) : `Not found`;
+};
