be0550e7b12ad4c942058a20160ddbec4e035276
Caio Rocha <rochacaiodev@gmail.com>
2023-08-25 12:40:27 -0300

    add more solved exercises

---
+++ JavaScript/7-kyu/greet-message.js
@@ -0,0 +1,14 @@
+/*
+
+Description:
+
+Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+Example:
+
+"riley" --> "Hello Riley!"
+"JACK"  --> "Hello Jack!"
+
+*/
+
+const greet = name => `Hello ${name.at(0).toUpperCase()}${name.substring(1).toLowerCase()}!`
